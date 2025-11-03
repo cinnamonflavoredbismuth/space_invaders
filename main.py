46c0cc40f04d70539b2307d02cfe750951c08a13
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-11-03 11:48:24 -0700

    started collision handling

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ How do you add sounds in pygame?
 """
 import pygame
 import random
+import math
 
 # Initialize Pygame
 pygame.init()
@@ -28,13 +29,31 @@ pygame_icon = pygame.image.load('resources/ufo.png')
 # 32 x 32 px image
 pygame.display.set_icon(pygame_icon)
 
+class Bullet:
+    def __init__(self,x=0,y=0):
+        self.img = pygame.image.load('resources/bullet.png')
+        self.x = x
+        self.y = y
+        self.change = -1
+        self.state = "ready"
+        self.rotated = pygame.transform.rotate(self.img, 90)
+        
+
+    def shoot(self):
+        screen.blit(self.rotated,(self.x,self.y))
+
+    def move(self):
+        self.y += self.change
+        if self.y <= 0:
+            self.state = "ready"
+
 class Player:
     def __init__(self,x=0,change=0):
         self.img = pygame.image.load('resources/spaceship.png')
         self.x = x
         self.y = (600 - 70)  # Position at the bottom of the screen
         self.change = change
-
+        
     def player_set(self):
         screen.blit(self.img,(self.x,self.y))
     
@@ -45,15 +64,44 @@ class Player:
         elif self.x >= 736:
             self.x = 736
 
+
+
 class Enemy:
-    def __init__(self,x,y,x_change=0,y_change=0):
+    def __init__(self,x=0,y=0):
         self.img = pygame.image.load('resources/alien.png')
         self.x = x
         self.y = y  # Position at the bottom of the screen
-        self.change_x = x_change
-        self.change_y = y_change
+        self.change_x = 0.1
+        self.change_y = 20
+    def enemy_set(self):
+        screen.blit(self.img,(self.x,self.y))
+    
+    def move(self):
+        self.x += self.change_x
+        # x boundary check
+        if self.x <= 0:
+            self.change_x= 0.1
+            self.y += self.change_y
+
+        elif self.x >= 736:
+            self.change_x= -0.1
+            self.y += self.change_y
+        
+        elif self.y >= 536:
+            self.y = 0
+    
+    def is_hit(self,bullet):
+        if math.sqrt((bullet.x-self.x)**2+(bullet.y-self.y)**2) < 27:
+            return True
+
+
+
 
 player = Player(370)
+enemny = Enemy(random.randint(0,800-64), random.randint(0,300-64))
+bullet = Bullet()
+
+
 
 
 # Main game loop
@@ -61,9 +109,10 @@ running = True
 while running:
     for event in pygame.event.get():
         keys = pygame.key.get_pressed()
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: # Close the window
             running = False
-        if event.type == pygame.KEYDOWN:
+
+        if event.type == pygame.KEYDOWN: # Key is pressed
 
             if keys[pygame.K_LEFT]:
                 player.change = -0.3
@@ -71,17 +120,32 @@ while running:
             if keys[pygame.K_RIGHT]:
                 player.change = 0.3
 
-        if event.type == pygame.KEYUP:
+            if keys[pygame.K_UP]:
+                
+                if bullet.state == "ready":
+                    bullet.state = "fire"
+                    bullet.x = player.x + 16
+                    bullet.y = player.y
+
+        if event.type == pygame.KEYUP: # Key is released
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player.change = 0
 
     # Movement
-    player.move()    
+
+    player.move()
+    enemny.move()
+   
+    bullet.move()    
 
     # Show Items
+
     screen.fill((0, 0, 0))  # Clear screen with black   
     #screen.blit(pygame.image.load('resources/background-1.jpg'), (0, 0))  # Background
     player.player_set()
+    enemny.enemy_set()
+    if bullet.state == "fire":
+        bullet.shoot()
     
 
     
